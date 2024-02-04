7a92e3f72bc560abfbf75e3a0f81a65d414c2ac0
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-02-04 06:44:16 +0200

    react state

---
+++ Week-9/reactstate/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import SpotCheck1 from "./components/events";
+function App() {
+  return (
+    <div className="App">
+      <SpotCheck1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ Week-9/reactstate/src/components/events.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const SpotCheck1 = () => {
+  const logHover = () => {
+    console.log("I was hovered!");
+  };
+
+  return <button onMouseEnter={logHover}>Click me</button>;
+};
+export default SpotCheck1;
