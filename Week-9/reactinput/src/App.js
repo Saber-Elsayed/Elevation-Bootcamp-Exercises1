7a281531d48c2194067d11c881a8bcf4ca5a00b2
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-02-04 11:39:01 +0200

    react input

---
+++ Week-9/reactinput/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import InputTest from "./components/input";
+function App() {
+  return (
+    <div className="App">
+      <InputTest />
+    </div>
+  );
+}
+
+export default App;
