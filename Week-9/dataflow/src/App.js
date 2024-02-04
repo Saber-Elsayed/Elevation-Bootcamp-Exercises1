6e40fe1ec6afadabb03eed98bf673a73460b6f29
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-02-04 08:51:43 +0200

    check spot 1

---
+++ Week-9/dataflow/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import SpotCheck from "./components/spot check1";
+function App() {
+  return (
+    <div className="App">
+      <SpotCheck />
+    </div>
+  );
+}
+
+export default App;
