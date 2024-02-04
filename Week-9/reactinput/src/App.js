236433207cad0e9903b377281020d1653f240a77
Saber-Elsayed <alsayyedsabe@gmail.com>
2024-02-04 11:51:26 +0200

    exe1

--- Week-9/reactinput/src/App.js
+++ Week-9/reactinput/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
 import InputTest from "./components/input";
+import Exercise1 from "./components/input";
+
 function App() {
   return (
     <div className="App">
-      <InputTest />
+      {/* <InputTest /> */}
+      <Exercise1 />
     </div>
   );
 }
---
+++ Week-9/reactinput/src/components/Exe1.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import { useState } from "react";
+
+const Exercise1 = () => {
+  const [person, setPerson] = useState({ name: "", age: "" });
+
+  const handleChange = (event, property) => {
+    setPerson({ ...person, [property]: event.target.value });
+  };
+  handleButtonCheck = () => {
+    alert(`${person.name}` + " your age is" + `${person.age}`);
+  };
+  return (
+    <div>
+      <input
+        id="name-input"
+        onChange={(e) => handleChange(e, "name")}
+        value={person.name}
+      />
+      <input
+        id="age-input"
+        onChange={(e) => handleChange(e, "age")}
+        value={person.age}
+      />
+      <button onClick={handleButtonCheck}>Go to Bar</button>
+    </div>
+  );
+};
+export default Exercise1;
